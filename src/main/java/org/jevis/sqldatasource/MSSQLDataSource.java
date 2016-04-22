d97135733729ae7f179c78b62a3c577cb77b4183
EnvidatecJM <jingxuan.man@envidatec.com>
2016-04-22 15:01:09 +0200

    Initial commit

---
+++ src/main/java/org/jevis/sqldatasource/MSSQLDataSource.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2015 NeroBurner; Copyright (C) 2016 JingxuanMan
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation in version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This driver is part of the OpenJEVis project, further project information are
+ * published at <http://www.OpenJEVis.org/>.
+ */
+package org.jevis.sqldatasource;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author NeroBurner (AIT)
+ * @author JingxuanMan (Envidatec)
+ */
+public class MSSQLDataSource extends SQLDriverAbstract {
+
+    interface MSSQL extends SQLDriverAbstract.SQLServer {
+
+        public final static String NAME = "MSSQL Server";
+    }
+
+    public String loadJDBC(String host, int port, String schema, String domain, String dbUser, String dbPW) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:jtds:sqlserver://" + host + ":" + port + "/" + schema;//+ ";instance=SQLEXPRESS;";
+        if (domain != null && !domain.isEmpty()) {
+            url += ";domain=" + domain;
+        }
+        Class.forName("net.sourceforge.jtds.jdbc.Driver");
+        System.out.println("Connection url: " + url);
+        _con = DriverManager.getConnection(url, dbUser, dbPW);
+
+        return url;
+    }
+
+    @Override
+    protected String getClassName() {
+        return MSSQL.NAME;
+    }
+}
---
+++ src/main/java/org/jevis/sqldatasource/MSSQLDataSourceMain.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) 2016 JingxuanMan
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation in version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * This driver is part of the OpenJEVis project, further project information are
+ * published at <http://www.OpenJEVis.org/>.
+ */
+package org.jevis.sqldatasource;
+
+import org.jevis.api.JEVisDataSource;
+import org.jevis.api.JEVisException;
+import org.jevis.api.JEVisObject;
+import org.jevis.api.sql.JEVisDataSourceSQL;
+
+/**
+ *
+ * @author JingxuanMan (Envidatec)
+ */
+public class MSSQLDataSourceMain {
+
+    public static void main(String[] args) throws JEVisException {
+        JEVisDataSource _client = new JEVisDataSourceSQL(
+                "openjevis.org", "13306", "jevis", "jevis", "jevistest");
+        _client.connect("Sys Admin", "JEV34Env");
+        JEVisObject mssqlServer = _client.getObject(4497l);
+        MSSQLDataSource dz = new MSSQLDataSource();
+        dz.initialize(mssqlServer);
+        dz.run();
+    }
+}
